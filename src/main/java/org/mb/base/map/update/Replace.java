4d504d4681e872493a74da4569d7f61982c8b081
moussbed bedril <moussakatbedril@yahoo.com>
2020-12-19 01:49:05 +0400

    Use new java 8 Map APIs to replace the existing value with condition

--- src/main/java/org/mb/base/map/update/Replace.java
+++ src/main/java/org/mb/base/map/update/Replace.java
@@ -3,12 +3,21 @@ package org.mb.base.map.update;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ReplaceExistingValueUnconditionally {
+public class Replace {
 
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
+
+        //Replace existing value unconditionally
         map.put("key1", "Bedroll");
         map.put("key1", "Moussaka's");
         System.out.println(map); //{key1=Moussakat}
+
+        // Replace existing value with condition
+        map.put("key2", "Anna");
+        map.put("key3", null);
+        map.putIfAbsent("key2", "Sidoine");
+        map.putIfAbsent("key4", "Eyala");
+        System.out.println(map);
     }
 }
