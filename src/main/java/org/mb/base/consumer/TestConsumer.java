9db4dff3bc2edc3a045e13de5c1887153ad6ae59
moussbed bedril <moussakatbedril@yahoo.com>
2020-12-18 21:34:53 +0400

    Add another functional interface Consumer who multiply by two each element in the list

--- src/main/java/org/mb/base/consumer/TestConsumer.java
+++ src/main/java/org/mb/base/consumer/TestConsumer.java
@@ -10,6 +10,14 @@ public class TestConsumer {
     public static void main(String[] args) {
         Consumer<List<Integer>> methodLambda = (List<Integer> l) -> Collections.sort(l);
         Consumer<List<Integer>> methodRef = Collections::reverse;
+        Consumer<List<Integer>> methodClassAnonym = new Consumer<List<Integer>>() {
+            @Override
+            public void accept(List<Integer> integers) {
+                for (int i = 0; i < integers.size(); i++) {
+                    integers.set(i, 2 * integers.get(i));
+                }
+            }
+        };
 
         List<Integer> list = Arrays.asList(56, 2, 34, 10, 45);
         System.out.println(list); // [56, 2, 34, 10, 45]
@@ -18,6 +26,9 @@ public class TestConsumer {
 
         methodRef.accept(list);
         System.out.println(list); // [56, 45, 34, 10, 2]
+
+        methodClassAnonym.accept(list); // [112, 90, 68, 20, 4]
+        System.out.println(list);
 
         Consumer<String> stringConsumer = System.out::println;
         stringConsumer.accept("Hello Word !"); // Hello Word !
