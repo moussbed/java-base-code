e2b94c59bb3ac39163ae7ee1c29c7a9bef190289
moussbed bedril <moussakatbedril@yahoo.com>
2020-12-18 23:56:15 +0400

    Base code functional interface Predicat for the removing conditional

---
+++ src/main/java/org/mb/base/predicat/removing/RemovingConditional.java
@@ -0,0 +1,24 @@
+package org.mb.base.predicat.removing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class RemovingConditional {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Tami");
+        list.add("Amina");
+        list.add("Richard");
+        System.out.println(list); // [Tami, Amina, Richard]
+
+        Predicate<String> predicate = (word)-> word.contains("m");
+        list.removeIf(predicate);
+        System.out.println(list); // [Richard]
+        
+        
+        
+    }
+
+}
